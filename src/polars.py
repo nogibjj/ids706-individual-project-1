28643ab8b67e2c9c6b0d5814aa662d649909f542
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-09-20 21:43:41 -0400

    Update polars.py

--- src/polars.py
+++ src/polars.py
@@ -1,4 +1,4 @@
-from lib import read_dataset, generate_summary_statistics
+from src.lib import read_dataset, generate_summary_statistics
 
 if __name__ == "__main__":
     data = read_dataset('../notebooks/winequality-red.csv')
